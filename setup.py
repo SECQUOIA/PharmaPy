6d27b4011b1f71d4f5cc85f67cd398a0520babf8
David E. Bernal Neira <dbernaln@purdue.edu>
2026-06-29 22:54:06 -0400

    Modernize packaging metadata

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup
 
-# Read the content of requirements.txt into a list
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
 
-setup(name='PharmaPy',
-      version='0.0.1',
-      packages=find_packages(),
-      author='Daniel Casas-Orozco',
-      author_email='dcasasor@purdue.edu',
-      license='',
-      url='',
-      py_modules=["PharmaPy"],
-      install_requires=requirements)
+setup()
