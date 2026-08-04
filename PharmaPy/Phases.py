7156e73655607a04693e74b866d5df49a2a2e622
test <test@test.local>
2026-08-04 13:08:34 -0400

    Fix SolidPhase mole accounting (#50)

--- PharmaPy/Phases.py
+++ PharmaPy/Phases.py
@@ -854,6 +854,12 @@ class SolidPhase(ThermoPhysicalManager):
     -------
     None.
 
+    Notes
+    -----
+    Solid mass is stored in kilograms, while molecular weight is stored in
+    grams per mole. Mole amounts are calculated from the finalized solid mass
+    after converting it to grams.
+
     """
     
     def __init__(self, path_thermo, temp=298.15, temp_ref=298.15, pres=101325,
@@ -923,8 +929,9 @@ class SolidPhase(ThermoPhysicalManager):
             else:
                 self.vol = self.mass / dens
 
-        mw_av = np.dot(self.mole_frac, self.mw)
-        self.moles = mass / mw_av
+        mw_av = np.dot(self.mole_frac, self.mw)  # [g/mol]
+        mass_grams = self.mass * 1000  # [g]
+        self.moles = mass_grams / mw_av  # [mol]
 
         if mass_frac is not None:
             sum_fracs = sum(mass_frac)
